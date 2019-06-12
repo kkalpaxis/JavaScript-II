6c8f1d9aab8a3f486c329e3a6a960b5b680303e1
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-06-11 22:10:04 -0400

    WIP: Array Methods Challenge 4 Complete; earlier Challenges Simplified.

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -66,31 +66,41 @@ console.log(fullName);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. 
 // Convert each first name into all caps and log the result
-let allCaps = [];
-runners.map(currentValue => {
-    allCaps.push(currentValue.first_name.toUpperCase());
-});
+let allCaps = runners
+.map((currentValue) => {
+    return currentValue.first_name.toUpperCase();
+    }
+);
 console.log(allCaps);
 
+// AfterHours example:
+//function firstUppercase (currentValue) {
+//    return currentValue.first_name.toUpperCase();
+//}
+//console.log(firstUppercase({first_name:"henry"}));
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  
 // Get a list of runners with large sized shirts so they can choose a different size. 
 // Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-runners.filter((currentValue) => {
-    if(currentValue.shirt_size === "L") {
-        largeShirts.push(currentValue.first_name + ' ' + currentValue.last_name);
+let largeShirts = runners
+.filter((currentValue) => {
+    return currentValue.shirt_size === "L"
     }
-});
+);
 console.log(largeShirts);
 
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. 
 // Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = runners
+.reduce((total, currentValue) => {
+   return total += currentValue.donation;
+}, 0);
 console.log(ticketPriceTotal);
+
+
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  
